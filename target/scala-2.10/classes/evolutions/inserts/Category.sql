cbf5becfb875a5df57721885d7d67b6f19d4cc13
Joakim Johansson <joakimjohansson@outlook.com>
2014-08-31 19:56:43 +0200

    Addedd gitignore

--- target/scala-2.10/classes/evolutions/inserts/Category.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `geomarket`.`category` (`NAME_ENG`, `NAME_SWE`) VALUES ('Food', 'Mat');
-INSERT INTO `geomarket`.`category` (`NAME_ENG`, `NAME_SWE`) VALUES ('Tickets', 'Biljetter');
-INSERT INTO `geomarket`.`category` (`NAME_ENG`, `NAME_SWE`) VALUES ('Discount', 'Rabatt');
