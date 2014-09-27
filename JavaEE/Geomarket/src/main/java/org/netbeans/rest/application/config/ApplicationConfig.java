04c7fa5f7779a69b644e50810f84b7681c6b900b
Joakim Johansson <joakimjohansson@outlook.com>
2014-09-27 20:38:04 +0200

    Jenkins test.. again

--- JavaEE/Geomarket/src/main/java/org/netbeans/rest/application/config/ApplicationConfig.java
+++ JavaEE/Geomarket/src/main/java/org/netbeans/rest/application/config/ApplicationConfig.java
@@ -16,7 +16,7 @@ import javax.ws.rs.core.Application;
 @javax.ws.rs.ApplicationPath("webresources")
 public class ApplicationConfig extends Application {
     
-    //Tjubaduba GitHubTest!
+    //Tjubaduba GitHubTest hello
    
     @Override
     public Set<Class<?>> getClasses() {
