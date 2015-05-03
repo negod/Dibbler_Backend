fc6af0cb882f016ddf93da388e88c604ee46c1e7
Joakim Johansson <joakimjohansson@outlook.com>
2015-05-03 18:20:32 +0200

    - Removed second CorrsFilter

--- Dibbler/src/main/java/se/dibbler/backend/service/filter/CORSFilterTest.java
+++ Dibbler/src/main/java/se/dibbler/backend/service/filter/CORSFilterTest.java
@@ -20,11 +20,11 @@ public class CORSFilterTest implements ContainerResponseFilter {
 
     @Override
     public void filter(ContainerRequestContext requestContext, ContainerResponseContext cres) throws IOException {
-        cres.getHeaders().add("Access-Control-Allow-Origin", "*");
+        /*cres.getHeaders().add("Access-Control-Allow-Origin", "*");
         cres.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization, X-Requested-With");
         cres.getHeaders().add("Access-Control-Allow-Credentials", "true");
         cres.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
-        cres.getHeaders().add("Access-Control-Max-Age", "1209600");
+        cres.getHeaders().add("Access-Control-Max-Age", "1209600");*/
     }
 
 }
